b552947e868ebe813451f55b9f6e744ed244e7e9
goDev <nguyentrongtiendev06@gmail.com>
2025-05-25 01:12:11 +0700

    update for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 import Login from "./pages/Login";
 import Projects from "./pages/project/Main";
-import Tasks from "./pages/task/Main.jsx";
+import Tasks from "./pages/task/Main";
 import WorkLogs from "./pages/WorkLogs";
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
